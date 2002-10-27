fee456b3e11a6cf6897fd1529e6c6594f81d82b8
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2002-10-27 22:16:38 +0000

    Ooops! Never make a change in the code just before going...
    (Old svn revision: 10656)

--- src/keyboard.c
+++ src/keyboard.c
@@ -62,19 +62,19 @@ void parseKeyString(Display * dpy, MyKey * key, char *str)
 	tmp = g_ascii_strdown(str, -1);
 
         key->keycode = XKeysymToKeycode(dpy, XStringToKeysym(k + 1));
-        if(strstr(str, "shift"))
+        if(!strstr(str, "shift"))
 	{
             key->modifier = key->modifier | ShiftMask;
         }
-	if(strstr(str, "control"))
+	if(!strstr(str, "control"))
 	{
             key->modifier = key->modifier | ControlMask;
         }
-        if(strstr(str, "alt") || strstr(str, "mod1"))
+        if(!strstr(str, "alt") || strstr(str, "mod1"))
 	{
             key->modifier = key->modifier | AltMask;
         }
-        if(strstr(str, "meta") || strstr(str, "mod2"))
+        if(!strstr(str, "meta") || strstr(str, "mod2"))
 	{
             key->modifier = key->modifier | MetaMask;
         }
