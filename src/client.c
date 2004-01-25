47caf10b322191d96feac8e4a3ff3a15a6eab981
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2004-01-25 14:16:13 +0000

    Rearrange grab/ungrab of button1
    (Old svn revision: 11545)

--- src/client.c
+++ src/client.c
@@ -115,6 +115,8 @@ static void clientGetInitialNetWmDesktop (Client * c);
 static void clientSetNetClientList (Atom a, GList * list);
 static void clientSetNetActions (Client * c);
 static void clientWindowType (Client * c);
+static void clientGrabButton1 (Client * c);
+static void clientUngrabButton1 (Client * c);
 static void clientAddToList (Client * c);
 static void clientRemoveFromList (Client * c);
 static void clientSetWidth (Client * c, int w1);
@@ -1406,7 +1408,6 @@ clientGrabButtons (Client * c)
     TRACE ("entering clientGrabButtons");
     TRACE ("grabbing buttons for client \"%s\" (0x%lx)", c->name, c->window);
     
-    grabButton(dpy, Button1, 0, c->window);
     grabButton(dpy, Button1, AltMask, c->window);
     grabButton(dpy, Button2, AltMask, c->window);
     grabButton(dpy, Button3, AltMask, c->window);
@@ -1417,22 +1418,42 @@ clientUngrabButtons (Client * c)
 {
     g_return_if_fail (c != NULL);
     TRACE ("entering clientUngrabButtons");
+    TRACE ("grabbing buttons for client \"%s\" (0x%lx)", c->name, c->window);
+    
+    XUngrabButton (dpy, AnyButton, AnyModifier, c->window);
+}
+
+static void
+clientGrabButton1 (Client * c)
+{
+    g_return_if_fail (c != NULL);
+    TRACE ("entering clientGrabButton1");
+    TRACE ("grabbing buttons for client \"%s\" (0x%lx)", c->name, c->window);
+    
+    grabButton(dpy, Button1, 0, c->window);
+}
+
+static void
+clientUngrabButton1 (Client * c)
+{
+    g_return_if_fail (c != NULL);
+    TRACE ("entering clientUngrabButton1");
     TRACE ("ungrabing buttons for client \"%s\" (0x%lx)", c->name, c->window);
 
     ungrabButton(dpy, Button1, 0, c->window);
 }
 
 void
-clientPassGrabButtons(Client * c)
+clientPassGrabButton1(Client * c)
 {
-    TRACE ("entering clientUngrabButtons");
+    TRACE ("entering clientPassGrabButton1");
     TRACE ("ungrabing buttons for client \"%s\" (0x%lx)", c->name, c->window);
 
     if (c == NULL)
     {
         if (last_ungrab)
         {
-            clientGrabButtons (last_ungrab);
+            clientGrabButton1 (last_ungrab);
         }
         last_ungrab = NULL;
         return;
@@ -1445,10 +1466,10 @@ clientPassGrabButtons(Client * c)
     
     if (last_ungrab)
     {
-        clientGrabButtons (last_ungrab);
+        clientGrabButton1 (last_ungrab);
     }
     
-    clientUngrabButtons (c);
+    clientUngrabButton1 (c);
     last_ungrab = c;
 }
 
@@ -2524,13 +2545,13 @@ clientConfigure (Client * c, XWindowChanges * wc, int mask, unsigned short flags
             case TopIf:
                 TRACE ("Above");
                 clientRaise (c);
-                clientPassGrabButtons (c);
+                clientPassGrabButton1 (c);
                 break;
             case Below:
             case BottomIf:
                 TRACE ("Below");
                 clientLower (c);
-                clientPassGrabButtons (NULL);
+                clientPassGrabButton1 (NULL);
                 break;
             case Opposite:
             default:
@@ -3026,11 +3047,11 @@ clientFocusNew(Client * c)
     if (params.focus_new || CLIENT_FLAG_TEST(c, CLIENT_FLAG_STATE_MODAL))
     {
         clientSetFocus (c, TRUE, FALSE);
-        clientPassGrabButtons (c);
+        clientPassGrabButton1 (c);
     }
     else
     {
-        clientPassGrabButtons (NULL);
+        clientPassGrabButton1 (NULL);
     }
 }
 
@@ -3386,9 +3407,12 @@ clientUnframe (Client * c, gboolean remap)
     {
         last_ungrab = NULL;
     }
+
     clientRemoveFromList (c);
     MyXGrabServer ();
     gdk_error_trap_push ();
+    clientUngrabKeys (c);
+    clientGrabButtons (c);
     XUnmapWindow (dpy, c->frame);
     clientGravitate (c, REMOVE);
     XSelectInput (dpy, c->window, NoEventMask);
@@ -3417,8 +3441,6 @@ clientUnframe (Client * c, gboolean remap)
         XDeleteProperty (dpy, c->window, net_wm_allowed_actions);
     }
     
-    clientUngrabKeys (c);
-    XUngrabButton (dpy, AnyButton, AnyModifier, c->window);
     myWindowDelete (&c->title);
     myWindowDelete (&c->sides[SIDE_LEFT]);
     myWindowDelete (&c->sides[SIDE_RIGHT]);
@@ -3817,7 +3839,7 @@ clientPassFocus (Client * c)
     clientSetFocus (new_focus, TRUE, TRUE);
     if (new_focus == top_most)
     {
-        clientPassGrabButtons (new_focus);
+        clientPassGrabButton1 (new_focus);
     }
 }
 
@@ -4191,7 +4213,7 @@ clientSetLayer (Client * c, int l)
         last_raise = NULL;
     }
     clientRaise (c);
-    clientPassGrabButtons (c);
+    clientPassGrabButton1 (c);
 }
 
 static inline void
@@ -4747,7 +4769,7 @@ clientUpdateFocus (Client * c)
             if (c)
             {
                 clientRaise(c);
-                clientPassGrabButtons (c);
+                clientPassGrabButton1 (c);
             }
         }
         frameDraw (c2, FALSE, FALSE);
@@ -4837,7 +4859,7 @@ clientSetFocus (Client * c, gboolean sort, gboolean ignore_modal)
             if (c)
             {
                 clientRaise(c);
-                clientPassGrabButtons (c);
+                clientPassGrabButton1 (c);
             }
         }
         TRACE ("redrawing previous focus client \"%s\" (0x%lx)", c2->name,
@@ -5903,7 +5925,7 @@ clientCycle (Client * c, XEvent * e)
         clientShow (passdata.c, TRUE);
         clientRaise (passdata.c);
         clientSetFocus (passdata.c, TRUE, FALSE);
-        clientPassGrabButtons (passdata.c);
+        clientPassGrabButton1 (passdata.c);
     }
 }
 
