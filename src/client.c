59f14acb1a348b132fb1d3b5ae633bf0702d0329
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2004-03-03 18:13:18 +0000

    Fix the circular linked list bug
    (Old svn revision: 11608)

--- src/client.c
+++ src/client.c
@@ -1225,7 +1225,6 @@ clientWindowType (Client * c)
         c2 = clientGetHighestTransientOrModalFor (c);
         if (c2)
         {
-#if 0
             if (clientIsTransient (c))
             {
                 c->initial_layer = c2->win_layer;
@@ -1234,9 +1233,6 @@ clientWindowType (Client * c)
             {
                 c->initial_layer = c2->win_layer;
             }
-#else
-            c->initial_layer = c2->win_layer;
-#endif
             TRACE ("Applied layer is %i", c->initial_layer);
         }
         FLAG_UNSET (c->flags,
@@ -4085,9 +4081,13 @@ clientRaise (Client * c)
                     transients = g_list_append (transients, c2);
                     if (sibling)
                     {
-                        /* Place the transient window just before sibling */
-                        windows_stack = g_list_remove (windows_stack, (gconstpointer) c2);
-                        windows_stack = g_list_insert_before (windows_stack, sibling, c2);
+                        /* Make sure client_sibling is not c2 otherwise we create a circular linked list */
+                        if (client_sibling != c2)
+                        {
+                            /* Place the transient window just before sibling */
+                            windows_stack = g_list_remove (windows_stack, (gconstpointer) c2);
+                            windows_stack = g_list_insert_before (windows_stack, sibling, c2);
+                        }
                     }
                     else
                     {
@@ -4098,8 +4098,7 @@ clientRaise (Client * c)
                 }
                 else
                 {
-                    for (index2 = transients; index2;
-                        index2 = g_list_next (index2))
+                    for (index2 = transients; index2; index2 = g_list_next (index2))
                     {
                         c3 = (Client *) index2->data;
                         if ((c3 != c2) && clientIsTransientOrModalFor (c2, c3))
@@ -4107,9 +4106,13 @@ clientRaise (Client * c)
                             transients = g_list_append (transients, c2);
                             if (sibling)
                             {
-                                /* Place the transient window just before sibling */
-                                windows_stack = g_list_remove (windows_stack, (gconstpointer) c2);
-                                windows_stack = g_list_insert_before (windows_stack, sibling, c2);
+                                /* Again, make sure client_sibling is not c2 to avoid a circular linked list */
+                                if (client_sibling != c2)
+                                {
+                                    /* Place the transient window just before sibling */
+                                    windows_stack = g_list_remove (windows_stack, (gconstpointer) c2);
+                                    windows_stack = g_list_insert_before (windows_stack, sibling, c2);
+                                }
                             }
                             else
                             {
