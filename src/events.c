8224cb45c3f7c83485508e159558669b66169047
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2004-03-04 23:12:06 +0000

    Partial fix for #0000128
    (Old svn revision: 11611)

--- src/events.c
+++ src/events.c
@@ -939,13 +939,13 @@ handleUnmapNotify (XUnmapEvent * ev)
     
     if (ev->from_configure)
     {
-        TRACE ("Ignoring UnmapNotify caused by parent's resize\n");
+        TRACE ("Ignoring UnmapNotify caused by parent's resize");
         return;
     }
 
     if ((ev->event != ev->window) && (ev->event != root || !ev->send_event))
     {
-        TRACE ("handleUnmapNotify (): Event ignored\n");
+        TRACE ("handleUnmapNotify (): Event ignored");
         return;
     }
 
@@ -1254,15 +1254,15 @@ handleFocusIn (XFocusChangeEvent * ev)
         /* Handle focus transition to root (means that an unknown
            window has vanished and the focus is returned to the root
          */
-         
         c = clientGetFocus ();
+        XSync(dpy, FALSE);
         if (c)
         {
             clientSetFocus (c, FOCUS_FORCE);
         }
         else
         {
-            clientUpdateFocus (c, FOCUS_NONE);
+            clientSetFocus (NULL, FOCUS_NONE);
             clientPassGrabButton1 (NULL);
         }
         return;
