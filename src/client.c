a968dbc2e0e8e5eca0973485c25e5a6af1683f45
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2004-04-14 17:02:40 +0000

    Fix focus transition from fullscreen as reported by Jean-Francois Wauthy <pollux@xfce.org>
    (Old svn revision: 11703)

--- src/client.c
+++ src/client.c
@@ -2002,6 +2002,12 @@ clientGetTopMostFocusable (int layer, Client * exclude)
         c = (Client *) index->data;
         TRACE ("*** stack window \"%s\" (0x%lx), layer %i", c->name,
             c->window, (int) c->win_layer);
+
+        if (c->type & (WINDOW_SPLASHSCREEN | WINDOW_DOCK | WINDOW_DESKTOP))
+        {
+            continue;
+        }
+        
         if (!exclude || (c != exclude))
         {
             if ((c->win_layer <= layer) && CLIENT_FLAG_TEST (c, CLIENT_FLAG_VISIBLE))
