febad9fa90dfe7262b1289e1b218380ff3d2704d
Jean-François Wauthy <pollux@xfce.org>
2004-07-15 08:50:37 +0000

    fix #219 in shortcut editor
    (Old svn revision: 11875)

--- mcs-plugin/xfwm4_shortcuteditor.c
+++ mcs-plugin/xfwm4_shortcuteditor.c
@@ -766,6 +766,9 @@ cb_browse_command (GtkWidget *widget, GtkEntry *entry_command)
 					    GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
 					    GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT, NULL);
 
+    xfce_file_chooser_set_filename (XFCE_FILE_CHOOSER (filesel_dialog),
+				    gtk_entry_get_text (entry_command));
+
     if(gtk_dialog_run (GTK_DIALOG (filesel_dialog)) == GTK_RESPONSE_ACCEPT)
     {
         gchar *filename;
