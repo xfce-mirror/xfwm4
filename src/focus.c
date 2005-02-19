a56ef89da2d682565a0b6065c1d022a9837977cc
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2005-02-19 14:00:32 +0000

    Use time stamp when focusing windows at first map.
    (Old svn revision: 12376)

--- src/focus.c
+++ src/focus.c
@@ -165,7 +165,7 @@ clientFocusNew(Client * c)
             }
         }
         clientSetFocus (screen_info, c, 
-                        CurrentTime, 
+                        myDisplayGetCurrentTime (screen_info->display_info), 
                         FOCUS_IGNORE_MODAL);
     }
     else
