c03349ee1685bddf4b7f74655ffc6bdf65c97e75
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2006-03-06 21:41:01 +0000

    SVN propset
    (Old svn revision: 20265)

--- mcs-plugin/xfwm4_plugin.c
+++ mcs-plugin/xfwm4_plugin.c
@@ -1,4 +1,4 @@
-/*      $Id: xfwm4_plugin.c 20089 2006-02-26 16:02:48Z olivier $
+/*      $Id$
  
         This program is free software; you can redistribute it and/or modify
         it under the terms of the GNU General Public License as published by
