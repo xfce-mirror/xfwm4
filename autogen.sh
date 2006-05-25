b48bfd33c3a3eef5f5d4c2c65216bb42571860d7
Jean-François Wauthy <pollux@xfce.org>
2006-05-25 18:40:49 +0000

    * add LINGUAS support * update svn:ignore
    (Old svn revision: 21840)

--- autogen.sh
+++ autogen.sh
@@ -23,10 +23,23 @@ EOF
   exit 1
 }
 
+# verify that po/LINGUAS is present
+(test -f po/LINGUAS) >/dev/null 2>&1 || {
+  cat >&2 <<EOF
+autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
+            or try to checkout again.
+EOF
+  exit 1
+}
+
 echo "Creating configure.ac"
-# substitute revision and date
+
+# substitute revision and linguas
+linguas=`sed -e '/^#/d' po/LINGUAS`
 revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}'`
-sed -e "s/@REVISION@/${revision}/g" < "configure.ac.in" > "configure.ac"
+sed -e "s/@LINGUAS@/${linguas}/g" \
+    -e "s/@REVISION@/${revision}/g" \
+    < "configure.ac.in" > "configure.ac"
 
 exec xdt-autogen $@
 
