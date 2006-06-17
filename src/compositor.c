ac4180178cae66bbc58209fb77e7ecd80c796c8a
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2006-06-17 16:37:40 +0000

    Sync after unredirect.
    (Old svn revision: 22145)

--- src/compositor.c
+++ src/compositor.c
@@ -972,6 +972,7 @@ unredirect_win (CWindow *cw)
         screen_info->overlays++;
         cw->redirected = FALSE;
         XCompositeUnredirectWindow (display_info->dpy, cw->id, display_info->composite_mode);
+        XSync (display_info->dpy, FALSE);
         TRACE ("Window 0x%lx unredirected", cw->id);
     }
 }
