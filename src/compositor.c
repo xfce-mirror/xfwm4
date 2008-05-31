1d51d00fd0e2ee0636b758b1d56d075e8655a427
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2008-05-31 14:35:42 +0000

    Update FSF address
    (Old svn revision: 26999)

--- src/compositor.c
+++ src/compositor.c
@@ -12,7 +12,9 @@
 
         You should have received a copy of the GNU General Public License
         along with this program; if not, write to the Free Software
-        Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+        Foundation, Inc., Inc., 51 Franklin Street, Fifth Floor, Boston,
+        MA 02110-1301, USA.
+
 
         xcompmgr - (c) 2003 Keith Packard
         metacity - (c) 2003, 2004 Red Hat, Inc.
