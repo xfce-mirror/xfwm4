81f8d7f8a49c0f42a6ad5570708163f92b679b22
Jannis Pohlmann <jannis@xfce.org>
2008-09-14 20:19:05 +0000

    * settings-dialogs/tweaks-settings.c: Add copyright nothint.
    (Old svn revision: 27873)

--- settings-dialogs/tweaks-settings.c
+++ settings-dialogs/tweaks-settings.c
@@ -1,5 +1,6 @@
 /*
  *  Copyright (c) 2008 Stephan Arts <stephan@xfce.org>
+ *  Copyright (c) 2008 Jannis Pohlmann <jannis@xfce.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
