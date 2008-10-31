d14cb036e95fe4cf2b1b4dd29ce42b22fd88ce5f
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2008-10-31 21:20:19 +0000

    * settings-dialogs/xfwm4-settings.c: Fix compiler warnings 	  xfwm4/trunk/settings-dialogs/Makefile.am 	  xfwm4/trunk/settings-dialogs/tweaks-settings.c 	  xfwm4/trunk/settings-dialogs/workspace-settings.c 	  xfwm4/trunk/settings-dialogs/xfwm4-settings.c 	  xfwm4/trunk/src/Makefile.am 	  xfwm4/trunk/src/event_filter.c 	  xfwm4/trunk/src/events.c 	  xfwm4/trunk/src/keyboard.c 	  xfwm4/trunk/src/keyboard.h 	  xfwm4/trunk/src/screen.h 	  xfwm4/trunk/src/settings.c 	  xfwm4/trunk/themes/Makefile.am 	  xfwm4/trunk/configure.ac.in 	  xfwm4/trunk/defaults/defaults: Add support for keyboard shortcuts. 	  Remove experimental shortcuts code and add dependency on 	  libxfce4kbd-private instead and replace MappingNotify event handler 	  with a GdkKeymap "keys-changed" callback. 	  Remove support for key themes. Properly stop startup notification 	  when dialogs are plugged into xfce4-settings-manager (Bug #4492). 	  po/*: Update translation files.
    (Old svn revision: 28542)

--- settings-dialogs/xfwm4-settings.c
+++ settings-dialogs/xfwm4-settings.c
@@ -39,6 +39,7 @@
 #include <glade/glade.h>
 
 #include <libxfce4util/libxfce4util.h>
+#include <libxfcegui4/libxfcegui4.h>
 #include <xfconf/xfconf.h>
 #include <libxfce4kbd-private/xfce-shortcut-dialog.h>
 #include <libxfce4kbd-private/xfce-shortcuts-provider.h>
@@ -1123,6 +1124,8 @@ xfwm_settings_active_frame_drag_motion (GtkWidget      *widget,
   active_box = glade_xml_get_widget (settings->priv->glade_xml, "active-box");
   children = gtk_container_get_children (GTK_CONTAINER (active_box));
 
+  /* Set a value so that the compiler does not (rightfully) complain */
+  ix = INDICATOR_SIZE;
   for (iter = children; iter != NULL; iter = g_list_next (iter))
     {
       if (GTK_WIDGET_VISIBLE (iter->data))
@@ -1815,7 +1818,6 @@ xfwm_settings_validate_shortcut (XfceShortcutDialog  *dialog,
   XfceShortcut          *other_shortcut = NULL;
   GList                 *providers;
   GList                 *iter;
-  gchar                 *property;
   gboolean               accepted = TRUE;
   gint                   response;
 
