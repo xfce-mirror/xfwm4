dbe4b0cfff338b8b16ab6b8f07377d4479a531c5
Olivier Fourdan <fourdan@xfce.org>
2009-09-04 14:02:45 +0200

    gitify the build generation

--- autogen.sh
+++ autogen.sh
@@ -36,14 +36,7 @@ echo "Creating configure.ac"
 
 # substitute revision and linguas
 linguas=`sed -e '/^#/d' po/LINGUAS`
-if test -d .git/svn; then
-  revision=`git svn find-rev trunk 2>/dev/null ||
-            git svn find-rev origin/trunk 2>/dev/null ||
-            git svn find-rev HEAD 2>/dev/null ||
-            git svn find-rev master 2>/dev/null`
-else
-  revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}'`
-fi
+revision=`git rev-parse --short HEAD`
 sed -e "s/@LINGUAS@/${linguas}/g" \
     -e "s/@REVISION@/${revision}/g" \
     < "configure.ac.in" > "configure.ac"
