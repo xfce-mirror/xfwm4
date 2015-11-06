xfce-mirror/xfwm4

commit 78b5c3c48a187ae54d97369a738b8a18e31e459a

Remove maximize button from non-resizable windows (bug #10910)
Signed-off-by: Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>

src/client.c
@@ -1149,6 +1149,10 @@ clientGetWMNormalHints (Client *c, gboolean update)
     {
         FLAG_SET (c->xfwm_flags, XFWM_FLAG_IS_RESIZABLE);
     }
+    else
+    {
+        FLAG_UNSET (c->xfwm_flags, XFWM_FLAG_HAS_MAXIMIZE | XFWM_FLAG_HAS_RESIZE);
+    }
 
     if (update)
     {

commit 46b7cfdcc1aa6126691d0443d0055234ab4c708e

Fix WM_SIZE_HINTS regression
Bug: 12176

If, upon mapping, WM_SIZE_HINTS specifies a non-resizable window, xfwm4
ignores all subsequent updates to WM_SIZE_HINTS set by the application.

This is a regression introduced by commit 78b5c3c (bug 10910).

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>

src/client.c
@@ -1215,10 +1215,6 @@ clientGetWMNormalHints (Client *c, gboolean update)
     {
         FLAG_SET (c->xfwm_flags, XFWM_FLAG_IS_RESIZABLE);
     }
-    else
-    {
-        FLAG_UNSET (c->xfwm_flags, XFWM_FLAG_HAS_MAXIMIZE | XFWM_FLAG_HAS_RESIZE);
-    }
 
     if (update)
     {

src/client.h
@@ -223,9 +223,8 @@
 #define CLIENT_CAN_HIDE_WINDOW(c)       (!(c->transient_for) && \
                                          FLAG_TEST(c->xfwm_flags, XFWM_FLAG_HAS_HIDE) && \
                                          !FLAG_TEST(c->flags, CLIENT_FLAG_SKIP_TASKBAR))
-#define CLIENT_CAN_MAXIMIZE_WINDOW(c)   (FLAG_TEST(c->xfwm_flags, XFWM_FLAG_HAS_MAXIMIZE | \
-                                                                  XFWM_FLAG_HAS_RESIZE | \
-                                                                  XFWM_FLAG_IS_RESIZABLE) && \
+#define CLIENT_CAN_MAXIMIZE_WINDOW(c)   (FLAG_TEST_ALL(c->xfwm_flags, XFWM_FLAG_HAS_MAXIMIZE | \
+                                                                      XFWM_FLAG_IS_RESIZABLE) && \
                                          !FLAG_TEST (c->flags, CLIENT_FLAG_FULLSCREEN))
 #define CLIENT_CAN_FILL_WINDOW(c)       (FLAG_TEST(c->xfwm_flags, XFWM_FLAG_HAS_RESIZE | \
                                                                   XFWM_FLAG_IS_RESIZABLE) && \