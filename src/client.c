xfce-mirror/xfwm4

commit 3877bf45d39928b2ec485945af1fba7a16645c7d

stacking: Raise on focus to activate
Bug: 12634

xfwm4 would raise a client upon receiving the _NET_ACTIVE_WINDOW message
but if the client provided an old timestamp, the window won't be focused
(as the timestamp will be older than the last current X server time).

To avoid this, only raise the window on focus event notification and
when using click-to-focus mode.

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>

src/client.c
@@ -2612,21 +2612,25 @@ clientActivate (Client *c, guint32 timestamp, gboolean source_is_application)
                 workspaceSwitch (screen_info, c->win_workspace, NULL, FALSE, timestamp);
             }
         }
-        clientRaise (sibling, None);
         clientShow (sibling, TRUE);
+        if (!screen_info->params->click_to_focus)
+        {
+            clientRaise (sibling, None);
+            clientSetLastRaise (c);
+        }
         if (!source_is_application || screen_info->params->click_to_focus || (c->type & WINDOW_TYPE_DONT_FOCUS))
         {
             /*
                It's a bit tricky here, we want to honor the activate request only if:
 
                - The window use the _NET_ACTIVE_WINDOW protocol and identify itself as a pager,
-               - Or we use the click to focus model, in that case we focus the raised window anyway,
+               - Or we use the click to focus model, in that case we focus the window anyway,
                - Or the request comes from an application that we would not focus by default,
                  such as panels for example
              */
+
             clientSetFocus (screen_info, c, timestamp, NO_FOCUS_FLAG);
         }
-        clientSetLastRaise (c);
     }
     else
     {

src/focus.c
@@ -482,6 +482,7 @@ void
 clientUpdateFocus (ScreenInfo *screen_info, Client * c, unsigned short flags)
 {
     Client *c2;
+    gboolean restacked;
 
     TRACE ("entering clientUpdateFocus");
 
@@ -522,7 +523,13 @@ clientUpdateFocus (ScreenInfo *screen_info, Client * c, unsigned short flags)
             FLAG_UNSET (c->flags, CLIENT_FLAG_DEMANDS_ATTENTION);
         }
         clientSetNetState (c);
-        clientAdjustFullscreenLayer (c, TRUE);
+        restacked = clientAdjustFullscreenLayer (c, TRUE);
+
+        if (!restacked && screen_info->params->click_to_focus)
+        {
+            clientRaise (c, None);
+            clientSetLastRaise (c);
+        }
         frameQueueDraw (c, FALSE);
         clientUpdateOpacity (c);
     }

commit a33fc7afbcfdddf20f29e60207c3b59ade9396d2

stacking: Raise ancestor on activate if different from window
Bug 14164

This is a regression introduced by 3877bf45, where we would differ
raising the window until it's focused.

When using click to focus, activating a transient window would raise
only that transient and not its ancestor, if any.

Change the logic to raise the ancestor if it's different from the
requested window itself.

src/client.c
@@ -2574,13 +2574,13 @@ clientActivate (Client *c, guint32 timestamp, gboolean source_is_application)
 {
     ScreenInfo *screen_info;
     Client *focused;
-    Client *sibling;
+    Client *ancestor;
 
     g_return_if_fail (c != NULL);
     TRACE ("entering clientActivate \"%s\" (0x%lx)", c->name, c->window);
 
     screen_info = c->screen_info;
-    sibling = clientGetTransientFor(c);
+    ancestor = clientGetTransientFor(c);
     focused = clientGetFocus ();
 
     if ((screen_info->current_ws == c->win_workspace) || (screen_info->params->activate_action != ACTIVATE_ACTION_NONE))
@@ -2606,10 +2606,10 @@ clientActivate (Client *c, guint32 timestamp, gboolean source_is_application)
                 workspaceSwitch (screen_info, c->win_workspace, NULL, FALSE, timestamp);
             }
         }
-        clientShow (sibling, TRUE);
-        if (!screen_info->params->click_to_focus)
+        clientShow (ancestor, TRUE);
+        if (c != ancestor)
         {
-            clientRaise (sibling, None);
+            clientRaise (ancestor, None);
             clientSetLastRaise (c);
         }
         if (!source_is_application || screen_info->params->click_to_focus || (c->type & WINDOW_TYPE_DONT_FOCUS))