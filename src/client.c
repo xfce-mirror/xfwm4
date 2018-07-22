0a5ba680f27a48242d52c590727deb837708da09
Olivier Fourdan <fourdan@xfce.org>
2018-07-22 10:54:35 +0200

    client: Check when GTK_FRAME_EXTENTS has changed
    Bug: 14511
    
    GTK+ will update its GTK_FRAME_EXTENTS property continuously even when
    the value hasn't actually changed, which causes the maximized state to
    be cleared when transitioning to/from fullscreen to maximized.
    
    Check that he extents have changed and reconfigure the client's window
    only when the value has changed.

--- src/client.c
+++ src/client.c
@@ -4021,6 +4021,7 @@ clientGetGtkFrameExtents (Client * c)
 {
     ScreenInfo *screen_info;
     DisplayInfo *display_info;
+    gboolean value_changed = FALSE;
     gulong *extents;
     int nitems;
     int i;
@@ -4040,7 +4041,11 @@ clientGetGtkFrameExtents (Client * c)
             FLAG_SET (c->flags, CLIENT_FLAG_HAS_FRAME_EXTENTS);
             for (i = 0; i < SIDE_COUNT; i++)
             {
-                c->frame_extents[i] = (int) extents[i];
+                if (c->frame_extents[i] != (int) extents[i])
+                {
+                    value_changed = TRUE;
+                    c->frame_extents[i] = (int) extents[i];
+                }
             }
         }
     }
@@ -4050,7 +4055,7 @@ clientGetGtkFrameExtents (Client * c)
         XFree (extents);
     }
 
-    return FLAG_TEST (c->flags, CLIENT_FLAG_HAS_FRAME_EXTENTS);
+    return value_changed;
 }
 
 gboolean
