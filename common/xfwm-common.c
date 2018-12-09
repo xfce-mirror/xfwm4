xfce-mirror/xfwm4

commit 9d5ac6a4f964ce1b4a12b402486f81c77cd3c5e1

Introduce xfwm-common static library

common/xfwm-common.c
@@ -18,7 +18,10 @@
  * MA 02110-1301, USA.
  */
 
-#include <gtk/gtk.h>
+#include <string.h>
+#include <gdk/gdkx.h>
+
+#include "xfwm-common.h"
 
 
 
@@ -44,3 +47,28 @@ xfwm_widget_reparent (GtkWidget *widget,
     }
 }
 
+
+
+void
+xfwm_get_screen_dimensions (gint *width, gint *height)
+{
+#if GTK_CHECK_VERSION(3, 22, 0)
+  GdkDisplay   *display;
+  GdkMonitor   *monitor;
+  GdkRectangle  geometry;
+
+  display = gdk_display_get_default ();
+  monitor = gdk_display_get_primary_monitor (display);
+  gdk_monitor_get_geometry (monitor, &geometry);
+
+  if (width != NULL)
+    *width = geometry.width;
+  if (height != NULL)
+    *height = geometry.height;
+#else
+  if (width != NULL)
+    *width = gdk_screen_width ();
+  if (height != NULL)
+    *height = gdk_screen_height ();
+#endif
+}

common/xfwm-common.h
@@ -23,7 +23,10 @@
 
 #include <gtk/gtk.h>
 
-void              xfwm_widget_reparent                  (GtkWidget *widget,
-                                                         GtkWidget *new_parent);
+void              xfwm_widget_reparent                  (GtkWidget    *widget,
+                                                         GtkWidget    *new_parent);
+
+void              xfwm_get_screen_dimensions            (gint         *width,
+                                                         gint         *height);
 
 #endif /* !__COMMON_H__ */

Makefile.am
@@ -17,6 +17,7 @@ SUBDIRS = 								\
 	helper-dialog 							\
 	icons 								\
 	po 								\
+	common 								\
 	settings-dialogs						\
 	src 								\
 	themes

common/Makefile.am
@@ -1,0 +1,20 @@
+AM_CPPFLAGS = \
+	-I${top_srcdir}
+
+noinst_LTLIBRARIES = \
+	libxfwm-common.la
+
+libxfwm_common_la_SOURCES = \
+	xfwm-common.c \
+	xfwm-common.h
+
+libxfwm_common_la_CFLAGS = \
+	$(GTK_CFLAGS) \
+	$(PLATFORM_CFLAGS)
+
+libxfwm_common_la_LIBADD = \
+	$(GTK_LIBS)
+
+libxfwm_common_la_LDFLAGS = \
+	-no-undefined \
+	$(PLATFORM_LDFLAGS)

configure.ac.in
@@ -284,6 +284,7 @@ icons/22x22/Makefile
 icons/48x48/Makefile
 icons/scalable/Makefile
 po/Makefile.in
+common/Makefile
 settings-dialogs/Makefile
 src/Makefile
 themes/Makefile

settings-dialogs/Makefile.am
@@ -7,9 +7,7 @@ xfwm4_workspace_settings_SOURCES = \
 	workspace-settings.c \
 	xfwm4-workspace-dialog_ui.h \
 	workspace-resource.c \
-	workspace-resource.h \
-	common.c \
-	common.h
+	workspace-resource.h
 
 xfwm4_workspace_settings_CFLAGS = \
 	$(GTK_CFLAGS) \
@@ -25,6 +23,7 @@ xfwm4_workspace_settings_CFLAGS = \
 	-DWNCK_I_KNOW_THIS_IS_UNSTABLE
 
 xfwm4_workspace_settings_LDADD = \
+	$(top_builddir)/common/libxfwm-common.la \
 	$(GTK_LIBS) \
 	$(GLIB_LIBS) \
 	$(DBUS_GLIB_LIBS) \
@@ -38,9 +37,7 @@ xfwm4_settings_SOURCES = \
 	xfwm4-settings.h \
 	xfwm4-dialog_ui.h \
 	range-debouncer.c \
-	range-debouncer.h \
-	common.c \
-	common.h
+	range-debouncer.h
 
 xfwm4_settings_CFLAGS = \
 	$(GTK_CFLAGS) \
@@ -54,6 +51,7 @@ xfwm4_settings_CFLAGS = \
 	-DLOCALEDIR=\"$(localedir)\"
 
 xfwm4_settings_LDADD = \
+	$(top_builddir)/common/libxfwm-common.la \
 	$(GTK_LIBS) \
 	$(GLIB_LIBS) \
 	$(DBUS_GLIB_LIBS) \
@@ -65,9 +63,7 @@ xfwm4_tweaks_settings_SOURCES = \
 	tweaks-settings.c \
 	xfwm4-tweaks-dialog_ui.h \
 	range-debouncer.c \
-	range-debouncer.h \
-	common.c \
-	common.h
+	range-debouncer.h
 
 xfwm4_tweaks_settings_CFLAGS = \
 	$(GTK_CFLAGS) \
@@ -80,6 +76,7 @@ xfwm4_tweaks_settings_CFLAGS = \
 	-DLOCALEDIR=\"$(localedir)\"
 
 xfwm4_tweaks_settings_LDADD = \
+	$(top_builddir)/common/libxfwm-common.la \
 	$(GTK_LIBS) \
 	$(GLIB_LIBS) \
 	$(DBUS_GLIB_LIBS) \
@@ -87,7 +84,8 @@ xfwm4_tweaks_settings_LDADD = \
 	$(LIBXFCONF_LIBS)
 
 AM_CPPFLAGS = \
-	-I${top_srcdir}
+	-I${top_srcdir} \
+	$(PLATFORM_CPPFLAGS)
 
 if MAINTAINER_MODE
 

settings-dialogs/tweaks-settings.c
@@ -43,9 +43,10 @@
 #include <libxfce4ui/libxfce4ui.h>
 #include <xfconf/xfconf.h>
 
+#include <common/xfwm-common.h>
+
 #include "xfwm4-tweaks-dialog_ui.h"
 #include "range-debouncer.h"
-#include "common.h"
 
 static Window opt_socket_id = 0;
 static gboolean opt_version = FALSE;

settings-dialogs/workspace-settings.c
@@ -37,8 +37,9 @@
 #include <libxfce4ui/libxfce4ui.h>
 #include <xfconf/xfconf.h>
 
+#include <common/xfwm-common.h>
+
 #include "xfwm4-workspace-dialog_ui.h"
-#include "common.h"
 
 #define WORKSPACES_CHANNEL         "xfwm4"
 
@@ -303,28 +304,6 @@ workspace_dialog_setup_names_treeview(GtkBuilder *builder,
 
 
 static void
-workspace_dialog_get_screen_dimensions (gint *width, gint *height)
-{
-#if GTK_CHECK_VERSION(3, 22, 0)
-    GdkDisplay   *display;
-    GdkMonitor   *monitor;
-    GdkRectangle  geometry;
-
-    display = gdk_display_get_default ();
-    monitor = gdk_display_get_primary_monitor (display);
-    gdk_monitor_get_geometry (monitor, &geometry);
-
-    *width = geometry.width;
-    *height = geometry.height;
-#else
-    *width = gdk_screen_width ();
-    *height = gdk_screen_height ();
-#endif
-}
-
-
-
-static void
 workspace_dialog_configure_widgets (GtkBuilder *builder,
                                     XfconfChannel *channel)
 {
@@ -352,7 +331,7 @@ workspace_dialog_configure_widgets (GtkBuilder *builder,
     }
 
     /* Set max margins range */
-    workspace_dialog_get_screen_dimensions (&wmax, &hmax);
+    xfwm_get_screen_dimensions (&wmax, &hmax);
     wmax /= 4;
     hmax /= 4;
 

commit 07773e608413a370932c5f2d59ed0b656240970a

tabwin: Place tabwin in center when display is scaled
Bug: 14931

On scaled display the tabwin is using unscaled window coordinates to calculate
its position. Let the callert choose scaled or unscaled display size and use
scaled in tabwin.

fixes: 9d5ac6a4 Introduce xfwm-common static library

Signed-off-by: Jonas Zetterberg <jozz@jozz.se>

common/xfwm-common.c
@@ -78,23 +78,26 @@ xfwm_get_screen_dimensions (gint *width, gint *height)
 void
 xfwm_get_monitor_geometry (GdkScreen    *screen,
                            gint          monitor_num,
-                           GdkRectangle *geometry)
+                           GdkRectangle *geometry,
+                           gboolean      scaled)
 {
 #if GTK_CHECK_VERSION(3, 22, 0)
   GdkDisplay *display;
   GdkMonitor *monitor;
-  int scale;
 
   display = gdk_screen_get_display (screen);
   monitor = gdk_display_get_monitor (display, monitor_num);
   gdk_monitor_get_geometry (monitor, geometry);
 
   /* Convert to device pixels */
-  scale = gdk_monitor_get_scale_factor (monitor);
-  geometry->x *= scale;
-  geometry->y *= scale;
-  geometry->width *= scale;
-  geometry->height *= scale;
+  if (scaled)
+    {
+      int scale = gdk_monitor_get_scale_factor (monitor);
+      geometry->x *= scale;
+      geometry->y *= scale;
+      geometry->width *= scale;
+      geometry->height *= scale;
+    }
 #else
   gdk_screen_get_monitor_geometry (screen, monitor_num, geometry);
 #endif

common/xfwm-common.h
@@ -31,7 +31,8 @@ void              xfwm_get_screen_dimensions            (gint         *width,
 
 void              xfwm_get_monitor_geometry             (GdkScreen    *screen,
                                                          gint          monitor_num,
-                                                         GdkRectangle *geometry);
+                                                         GdkRectangle *geometry,
+                                                         gboolean      scaled);
 
 void              xfwm_get_primary_monitor_geometry     (GdkScreen    *screen,
                                                          GdkRectangle *geometry);

src/screen.c
@@ -642,7 +642,7 @@ myScreenComputeSize (ScreenInfo *screen_info)
 
     for (i = 0; i < num_monitors; i++)
     {
-        xfwm_get_monitor_geometry (screen_info->gscr, i, &monitor);
+        xfwm_get_monitor_geometry (screen_info->gscr, i, &monitor, TRUE);
         width = MAX (monitor.x + monitor.width, width);
         height = MAX (monitor.y + monitor.height, height);
     }
@@ -717,11 +717,11 @@ myScreenRebuildMonitorIndex (ScreenInfo *screen_info)
     num_monitors = xfwm_get_n_monitors (screen_info->gscr);
     for (i = 0; i < num_monitors; i++)
     {
-        xfwm_get_monitor_geometry (screen_info->gscr, i, &monitor);
+        xfwm_get_monitor_geometry (screen_info->gscr, i, &monitor, TRUE);
         cloned = FALSE;
         for (j = 0; j < (gint) screen_info->monitors_index->len; j++)
         {
-            xfwm_get_monitor_geometry (screen_info->gscr, j, &previous);
+            xfwm_get_monitor_geometry (screen_info->gscr, j, &previous, TRUE);
             if ((previous.x == monitor.x) && (previous.y == monitor.y))
             {
                 cloned = TRUE;
@@ -784,7 +784,7 @@ myScreenFindMonitorAtPoint (ScreenInfo *screen_info, gint x, gint y, GdkRectangl
         gint monitor_index;
 
         monitor_index = myScreenGetMonitorIndex (screen_info, i);
-        xfwm_get_monitor_geometry (screen_info->gscr, monitor_index, &monitor);
+        xfwm_get_monitor_geometry (screen_info->gscr, monitor_index, &monitor, TRUE);
 
         if ((x >= monitor.x) && (x < (monitor.x + monitor.width)) &&
             (y >= monitor.y) && (y < (monitor.y + monitor.height)))

src/tabwin.c
@@ -329,7 +329,7 @@ getMinMonitorWidth (ScreenInfo *screen_info)
     for (min_width = i = 0; i < num_monitors; i++)
     {
         GdkRectangle monitor;
-        xfwm_get_monitor_geometry (screen_info->gscr, i, &monitor);
+        xfwm_get_monitor_geometry (screen_info->gscr, i, &monitor, FALSE);
         if (min_width == 0 || monitor.width < min_width)
             min_width = monitor.width;
     }
@@ -343,7 +343,7 @@ getMinMonitorHeight (ScreenInfo *screen_info)
     for (min_height = i = 0; i < num_monitors; i++)
     {
         GdkRectangle monitor;
-        xfwm_get_monitor_geometry (screen_info->gscr, i, &monitor);
+        xfwm_get_monitor_geometry (screen_info->gscr, i, &monitor, FALSE);
         if (min_height == 0 || monitor.height < min_height)
         {
             min_height = monitor.height;
@@ -573,7 +573,7 @@ tabwinConfigure (TabwinWidget *tabwin_widget, GdkEventConfigure *event)
     }
 
     xfwm_get_monitor_geometry (gtk_widget_get_screen (GTK_WIDGET (tabwin_widget)),
-                               tabwin_widget->monitor_num, &monitor);
+                               tabwin_widget->monitor_num, &monitor, FALSE);
     x = monitor.x + (monitor.width - event->width) / 2;
     y = monitor.y + (monitor.height - event->height) / 2;
     gtk_window_move (GTK_WINDOW (tabwin_widget), x, y);
@@ -784,7 +784,7 @@ tabwinCreateWidget (Tabwin *tabwin, ScreenInfo *screen_info, gint monitor_num)
                                     MAX (border.left, MAX (border.top, (MAX (border.right, border.bottom)))) +
                                     MAX (padding.left, MAX (padding.top, (MAX (padding.right, padding.bottom)))));
     gtk_window_set_position (GTK_WINDOW (tabwin_widget), GTK_WIN_POS_NONE);
-    xfwm_get_monitor_geometry (screen_info->gscr, tabwin_widget->monitor_num, &monitor);
+    xfwm_get_monitor_geometry (screen_info->gscr, tabwin_widget->monitor_num, &monitor, FALSE);
     gtk_window_move (GTK_WINDOW (tabwin_widget), monitor.x + monitor.width / 2,
                                       monitor.y + monitor.height / 2);
 