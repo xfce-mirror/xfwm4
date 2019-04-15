xfce-mirror/xfwm4

commit 3877bf45d39928b2ec485945af1fba7a16645c7d

stacking: Raise on focus to activate
Bug: 12634

xfwm4 would raise a client upon receiving the _NET_ACTIVE_WINDOW message
but if the client provided an old timestamp, the window won't be focused
(as the timestamp will be older than the last current X server time).

To avoid this, only raise the window on focus event notification and
when using click-to-focus mode.

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>

src/focus.c
@@ -482,6 +482,7 @@ void
 clientUpdateFocus (ScreenInfo *screen_info, Client * c, unsigned short flags)
 {
     Client *c2;
+    gboolean restacked;
 
     TRACE ("entering clientUpdateFocus");
 
@@ -522,7 +523,13 @@ clientUpdateFocus (ScreenInfo *screen_info, Client * c, unsigned short flags)
             FLAG_UNSET (c->flags, CLIENT_FLAG_DEMANDS_ATTENTION);
         }
         clientSetNetState (c);
-        clientAdjustFullscreenLayer (c, TRUE);
+        restacked = clientAdjustFullscreenLayer (c, TRUE);
+
+        if (!restacked && screen_info->params->click_to_focus)
+        {
+            clientRaise (c, None);
+            clientSetLastRaise (c);
+        }
         frameQueueDraw (c, FALSE);
         clientUpdateOpacity (c);
     }

src/client.c
@@ -2612,21 +2612,25 @@ clientActivate (Client *c, guint32 timestamp, gboolean source_is_application)
                 workspaceSwitch (screen_info, c->win_workspace, NULL, FALSE, timestamp);
             }
         }
-        clientRaise (sibling, None);
         clientShow (sibling, TRUE);
+        if (!screen_info->params->click_to_focus)
+        {
+            clientRaise (sibling, None);
+            clientSetLastRaise (c);
+        }
         if (!source_is_application || screen_info->params->click_to_focus || (c->type & WINDOW_TYPE_DONT_FOCUS))
         {
             /*
                It's a bit tricky here, we want to honor the activate request only if:
 
                - The window use the _NET_ACTIVE_WINDOW protocol and identify itself as a pager,
-               - Or we use the click to focus model, in that case we focus the raised window anyway,
+               - Or we use the click to focus model, in that case we focus the window anyway,
                - Or the request comes from an application that we would not focus by default,
                  such as panels for example
              */
+
             clientSetFocus (screen_info, c, timestamp, NO_FOCUS_FLAG);
         }
-        clientSetLastRaise (c);
     }
     else
     {

commit 7431908b17d3e75d7f5ac9b55fae2d24194380ca

focus: Raise on activate only with raise-on-focus
Bug: 14656

Commit 3877bf4 ("stacking: Raise on focus to activate") caused a
regression with raise-on-focus disabled as it would always raise
regardless of the actual value of the setting.

Raise window on activate only when "raise-on-focus" is enabled.

src/focus.c
@@ -525,7 +525,7 @@ clientUpdateFocus (ScreenInfo *screen_info, Client * c, unsigned short flags)
         clientSetNetState (c);
         restacked = clientAdjustFullscreenLayer (c, TRUE);
 
-        if (!restacked && screen_info->params->click_to_focus)
+        if (!restacked && screen_info->params->raise_on_focus)
         {
             clientRaise (c, None);
             clientSetLastRaise (c);