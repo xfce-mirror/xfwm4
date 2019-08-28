91133dfb0d65e5c2a720ab0700e12833f1f64cc3
Olivier Fourdan <fourdan@xfce.org>
2019-08-28 10:28:16 -0600

    compositor: Don't repaint a screen of zero size
    Bug: 15852
    
    If no monitor is connected for a given screen, it's reported size will
    be 0.
    
    In that case, trying to repaint the screen using GLX for vsync will
    crash.
    
    Add a test to make sure the size of the screen is a thing, before
    queuing up a repaint.
    
    Signed-off-by: Olivier Fourdan <fourdan@xfce.org>
    (cherry picked from commit 3925109d09614f95a5ef96b35db686d889457af0)
    (cherry picked from commit 25eca2752133467d1cdb366c9557ebd62fb28143)

--- src/compositor.c
+++ src/compositor.c
@@ -2389,6 +2389,11 @@ repair_screen (ScreenInfo *screen_info)
         return FALSE;
     }
 
+    if (screen_info->width == 0 || screen_info->height == 0)
+    {
+        return FALSE;
+    }
+
     display_info = screen_info->display_info;
     damage = screen_info->allDamage;
     if (damage)
@@ -2592,6 +2597,11 @@ damage_screen (ScreenInfo *screen_info)
     DisplayInfo *display_info;
     XserverRegion region;
     XRectangle  r;
+
+    if (screen_info->width == 0 || screen_info->height == 0)
+    {
+        return;
+    }
 
     display_info = screen_info->display_info;
     r.x = 0;
