xfce-mirror/xfwm4

commit 49c1a33a4dfe15f312334584c22b4ec3ed792214

client: Rely in XRes reported PID is available
When xfwm4 needs to kill an unresponsive client, it will use the its PID
is known.

However, the _NET_WM_PID as defined in the extended window manager hint
mechanism is unreliable when the client runs within a sandbox, as it is
the client itself who sets that property using its own view of the PID.

The Xserver can provide a more accurate value of the client PID using
the XRes X11 extension, so use that protocol if available.

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>

src/client.c
@@ -17,7 +17,7 @@
 
 
         oroborus - (c) 2001 Ken Lynch
-        xfwm4    - (c) 2002-2011 Olivier Fourdan
+        xfwm4    - (c) 2002-2020 Olivier Fourdan
 
  */
 
@@ -1793,9 +1793,8 @@ clientFrame (DisplayInfo *display_info, Window w, gboolean recapture)
     clientAddUserTimeWin (c);
     clientGetUserTime (c);
 
-    /*client PID */
-    getHint (display_info, c->window, NET_WM_PID, (long *) &pid);
-    c->pid = (GPid) pid;
+    /* client PID */
+    c->pid = getWindowPID (display_info, c->window);
     TRACE ("client \"%s\" (0x%lx) PID = %i", c->name, c->window, c->pid);
 
     /* Apply startup notification properties if available */

src/events.c
@@ -17,7 +17,7 @@
 
 
         oroborus - (c) 2001 Ken Lynch
-        xfwm4    - (c) 2002-2015 Olivier Fourdan
+        xfwm4    - (c) 2002-2020 Olivier Fourdan
 
  */
 
@@ -581,6 +581,8 @@ handleKeyPress (DisplayInfo *display_info, XfwmEventKey *event)
 static eventFilterStatus
 handleKeyRelease (DisplayInfo *display_info, XfwmEventKey *event)
 {
+    Client *c;
+
     TRACE ("entering");
 
     /* Release pending events */
@@ -1621,6 +1623,7 @@ handleFocusIn (DisplayInfo *display_info, XFocusChangeEvent * ev)
         }
     }
 
+
     return EVENT_FILTER_REMOVE;
 }
 
@@ -1804,11 +1807,12 @@ handlePropertyNotify (DisplayInfo *display_info, XPropertyEvent * ev)
         }
         else if (ev->atom == display_info->atoms[NET_WM_PID])
         {
-            long pid;
             TRACE ("client \"%s\" (0x%lx) has received a NET_WM_PID notify", c->name, c->window);
-            getHint (display_info, c->window, NET_WM_PID, (long *) &pid);
-            c->pid = (GPid) pid;
-            TRACE ("client \"%s\" (0x%lx) updated PID = %i", c->name, c->window, c->pid);
+            if (c->pid == 0)
+            {
+                c->pid = getWindowPID (display_info, c->window);
+                TRACE ("client \"%s\" (0x%lx) updated PID = %i", c->name, c->window, c->pid);
+            }
         }
         else if (ev->atom == display_info->atoms[NET_WM_WINDOW_OPACITY])
         {

commit 06707bc2b9c87eb77d093fe8041bae09472f6062

clients: Fix a crash on FreeBSD
A pid is not necessarily a long on all systems, so casting the value of
NET_WM_PID directly into a GPid may cause a stack overflow.

Fixes: 49c1a33a
Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/411
Signed-off-by: Olivier Fourdan <fourdan@xfce.org>

src/client.c
@@ -1596,7 +1596,6 @@ clientFrame (DisplayInfo *display_info, Window w, gboolean recapture)
     gboolean shaped;
     gchar *wm_name;
     unsigned long valuemask;
-    long pid;
     int i;
 
     g_return_val_if_fail (w != None, NULL);

src/hints.c
@@ -1450,7 +1450,7 @@ getWindowStartupId (DisplayInfo *display_info, Window w, gchar **startup_id)
 GPid
 getWindowPID (DisplayInfo *display_info, Window w)
 {
-    GPid pid = 0;
+    long pid = 0;
 #ifdef HAVE_XRES
     XResClientIdSpec client_specs;
     XResClientIdValue *client_ids = NULL;
@@ -1476,7 +1476,7 @@ getWindowPID (DisplayInfo *display_info, Window w)
                 if (client_ids[i].spec.mask == XRES_CLIENT_ID_PID_MASK)
                 {
                     CARD32 *value = client_ids[i].value;
-                    pid = (GPid) *value;
+                    pid = (long) *value;
                     break;
                 }
             }
@@ -1485,7 +1485,7 @@ getWindowPID (DisplayInfo *display_info, Window w)
 
             if (pid > 0)
             {
-                return pid;
+                return (GPid) pid;
             }
         }
     }
@@ -1493,5 +1493,5 @@ getWindowPID (DisplayInfo *display_info, Window w)
 
     getHint (display_info, w, NET_WM_PID, (long *) &pid);
 
-    return pid;
+    return (GPid) pid;
 }