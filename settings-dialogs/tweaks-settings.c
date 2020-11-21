65c168ab5ef49e817c84b8c8c6e26246be128010
Olivier Fourdan <fourdan@xfce.org>
2020-11-21 20:57:59 +0100

    settings: Add a UI option for desktop zooming
    Signed-off-by: Olivier Fourdan <fourdan@xfce.org>

--- settings-dialogs/tweaks-settings.c
+++ settings-dialogs/tweaks-settings.c
@@ -215,6 +215,7 @@ wm_tweaks_dialog_configure_widgets (GtkBuilder *builder)
     GtkWidget *show_frame_shadow_check = GTK_WIDGET (gtk_builder_get_object (builder, "show_frame_shadow_check"));
     GtkWidget *show_popup_shadow_check = GTK_WIDGET (gtk_builder_get_object (builder, "show_popup_shadow_check"));
     GtkWidget *show_dock_shadow_check = GTK_WIDGET (gtk_builder_get_object (builder, "show_dock_shadow_check"));
+    GtkWidget *zoom_desktop_check = GTK_WIDGET (gtk_builder_get_object (builder, "zoom_desktop_check"));
     GtkWidget *zoom_pointer_check = GTK_WIDGET (gtk_builder_get_object (builder, "zoom_pointer_check"));
 
     GtkWidget *frame_opacity_scale = GTK_WIDGET (gtk_builder_get_object (builder, "frame_opacity_scale"));
@@ -425,6 +426,10 @@ wm_tweaks_dialog_configure_widgets (GtkBuilder *builder)
                             "/general/show_dock_shadow",
                             G_TYPE_BOOLEAN,
                             (GObject *)show_dock_shadow_check, "active");
+    xfconf_g_property_bind (xfwm4_channel,
+                            "/general/zoom_desktop",
+                            G_TYPE_BOOLEAN,
+                            (GObject *)zoom_desktop_check, "active");
     xfconf_g_property_bind (xfwm4_channel,
                             "/general/zoom_pointer",
                             G_TYPE_BOOLEAN,
