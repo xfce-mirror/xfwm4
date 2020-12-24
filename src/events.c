e5aa523722987a6d96976340094fa76c9cff7dbe
sfan5 <sfan5@live.de>
2020-12-24 18:29:34 +0100

    events: Protect against error from XGetWMHints()
    (cherry picked from commit bfd7c46dce086555e3f82f65511fe2200406d239)

--- src/events.c
+++ src/events.c
@@ -1728,7 +1728,10 @@ handlePropertyNotify (DisplayInfo *display_info, XPropertyEvent * ev)
                 XFree (c->wmhints);
             }
 
+            myDisplayErrorTrapPush (display_info);
             c->wmhints = XGetWMHints (display_info->dpy, c->window);
+            myDisplayErrorTrapPopIgnored (display_info);
+
             if (c->wmhints)
             {
                 if (c->wmhints->flags & WindowGroupHint)
