xfce-mirror/xfwm4

commit 2fa246ca1e67be08f4d436eaefb8317af77da2cc

compositor: Add define for compositor_timeout_cb interval
The call to g_timeout_add in add_repair seems to be mixing up priority
and callback interval.  Instead, let's call g_timeout_add_full to make
it clear there are two separate fields, and add an appropriate define
for the callback interval, setting it to the same 1ms value it was
currently using.

Signed-off-by: Michael Banack <bob5972@banack.net>

src/compositor.c
@@ -102,6 +102,10 @@
 #define TIMEOUT_REPAINT_PRIORITY   1
 #endif /* TIMEOUT_REPAINT_PRIORITY */
 
+#ifndef TIMEOUT_REPAINT_MS
+#define TIMEOUT_REPAINT_MS   1
+#endif /* TIMEOUT_REPAINT_MS */
+
 #ifndef MONITOR_ROOT_PIXMAP
 #define MONITOR_ROOT_PIXMAP   1
 #endif /* MONITOR_ROOT_PIXMAP */
@@ -2496,8 +2500,9 @@ add_repair (ScreenInfo *screen_info)
     if (screen_info->compositor_timeout_id == 0)
     {
         screen_info->compositor_timeout_id =
-            g_timeout_add (G_PRIORITY_DEFAULT + TIMEOUT_REPAINT_PRIORITY,
-                           compositor_timeout_cb, screen_info);
+            g_timeout_add_full (G_PRIORITY_DEFAULT + TIMEOUT_REPAINT_PRIORITY,
+                                TIMEOUT_REPAINT_MS,
+                                compositor_timeout_cb, screen_info, NULL);
     }
 }
 

commit 88c735c94ff881254d3477fe77d30aed7b5edf75

compositor: Fix repaint timeout
Commit 2fa246ca ("Add define for compositor_timeout_cb interval")
replaced g_timeout_add() with g_timeout_add_full() to separate the
timeout and priority.

Unfortunately, in doing so, it confused the timeout and priority and
decreased the priority slightly, causing the repaint timeout to occur
less often, causing delays in repaint.

Fix the priority and timeout as intended.

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>
Fixes: 2fa246ca - Add define for compositor_timeout_cb interval
Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/503

src/compositor.c
@@ -103,7 +103,7 @@
 #define WIN_IS_SHADED(cw)               (WIN_HAS_CLIENT(cw) && FLAG_TEST (cw->c->flags, CLIENT_FLAG_SHADED))
 
 #ifndef TIMEOUT_REPAINT_PRIORITY
-#define TIMEOUT_REPAINT_PRIORITY   1
+#define TIMEOUT_REPAINT_PRIORITY   G_PRIORITY_DEFAULT
 #endif /* TIMEOUT_REPAINT_PRIORITY */
 
 #ifndef TIMEOUT_REPAINT_MS
@@ -2753,7 +2753,7 @@ add_repair (ScreenInfo *screen_info)
     if (screen_info->compositor_timeout_id == 0)
     {
         screen_info->compositor_timeout_id =
-            g_timeout_add_full (G_PRIORITY_DEFAULT + TIMEOUT_REPAINT_PRIORITY,
+            g_timeout_add_full (TIMEOUT_REPAINT_PRIORITY,
                                 TIMEOUT_REPAINT_MS,
                                 compositor_timeout_cb, screen_info, NULL);
     }