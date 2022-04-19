xfce-mirror/xfwm4

commit 50d34206fd3f01e71a16627821bbecc90c53a95e

compositor: Damage opaque region if changed
When using multi buffers (Present, GLX), if the client updates its
opaque region in between (e.g. when resizing the window) we may cull out
regions on screen that won't get updated, leaving trails of unpainted
areas.

To avoid that, make sure we add damage for the delta in opaque region.

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>

src/compositor.c
@@ -2971,6 +2971,7 @@ update_opaque_region (CWindow *cw, Window id)
     ScreenInfo *screen_info;
     XRectangle *rects = NULL;
     unsigned int nrects;
+    XserverRegion old_opaque_region;
 
     g_return_if_fail (cw != NULL);
     TRACE ("window 0x%lx", cw->id);
@@ -2978,11 +2979,7 @@ update_opaque_region (CWindow *cw, Window id)
     screen_info = cw->screen_info;
     display_info = screen_info->display_info;
 
-    if (cw->opaque_region)
-    {
-        XFixesDestroyRegion (display_info->dpy, cw->opaque_region);
-        cw->opaque_region = None;
-    }
+    old_opaque_region = cw->opaque_region;
 
     nrects = getOpaqueRegionRects (display_info, id, &rects);
     if (nrects)
@@ -2990,8 +2987,24 @@ update_opaque_region (CWindow *cw, Window id)
         cw->opaque_region = XFixesCreateRegion (display_info->dpy, rects, nrects);
         g_free (rects);
     }
+    else
+    {
+        cw->opaque_region = None;
+    }
 
-    damage_win (cw);
+    if (old_opaque_region)
+    {
+        if (cw->opaque_region)
+        {
+            XFixesSubtractRegion (display_info->dpy, old_opaque_region,
+                                  old_opaque_region, cw->opaque_region);
+        }
+        XFixesTranslateRegion (display_info->dpy, old_opaque_region,
+                               cw->attr.x + cw->attr.border_width,
+                               cw->attr.y + cw->attr.border_width);
+        /* old_opaque_region region will be destroyed by add_damage () */
+        add_damage (screen_info, old_opaque_region);
+    }
 }
 
 static void

commit 0bacec27bd35843dc4994fc0f53191c604ac566b

compositor: Damage window when adding the opaque region
When mapping a new window for the first time, the client may set an
opaque region prior to draw anything.

As opaquere regions are meant to hint the compositor that anything
underneath doesn't need updating (as hidden behind an opaque region,
precisely), xfwm4 would leave that region untouched.

However, when using double buffering as with xpresent, it means that
content of the different buffers may end up being different if there is
any damage occurring underneath the opaque region, before the client
itself has painted anything.

As the buffers alternate quickly, the content behind the opaque region
will show alternatively which is ugly (although technically correct).

To avoid that issue, when setting the opaque region first, make sure to
damage the whole window so that the content of the different buffers
remain updated even if behind the opaque region.

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>
Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/583
Fixes: 50d34206 - Damage opaque region if changed

src/compositor.c
@@ -3268,6 +3268,10 @@ update_opaque_region (CWindow *cw, Window id)
             add_damage (screen_info, old_opaque_region);
         }
     }
+    else
+    {
+        damage_win (cw);
+    }
 }
 
 static void