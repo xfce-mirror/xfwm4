xfce-mirror/xfwm4

commit 7e8d60bb2261a3913cabf67751bd1060e80b9442

placement: Ignore struts from windows on other monitors
The EWMH[1] “specification” defines truts are relative to the entire X11
screen, which include all monitors.

Under some configuration, when montors are placed one on top of the
other, that struts set on the monitor above would invalidate the entire
lower monitor, preventing from placing windows on that monitors.

To avoid that issue, xfwm4 was computing the actual struts area and
would simply ignore any struts greater than a quarter of the screen.

This is largely inefficient and only very partially solve the problem.

Instead, simply apply struts for windows on that monitor and ignore
those from the other monitors, so that a panel placed on the monitors
above would simply be ignored on the monitor below.

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>
Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/517

src/workspaces.c
@@ -45,6 +45,7 @@
 #include "settings.h"
 #include "client.h"
 #include "focus.h"
+#include "frame.h"
 #include "stacking.h"
 #include "hints.h"
 
@@ -523,7 +524,7 @@ workspaceUpdateArea (ScreenInfo *screen_info)
 {
     DisplayInfo *display_info;
     Client *c;
-    GdkRectangle top, left, right, bottom, workarea;
+    GdkRectangle top, left, right, bottom, workarea, win_area;
     int prev_top;
     int prev_left;
     int prev_right;
@@ -556,6 +557,16 @@ workspaceUpdateArea (ScreenInfo *screen_info)
 
     for (c = screen_info->clients, i = 0; i < screen_info->client_count; c = c->next, i++)
     {
+        win_area.x = frameExtentX (c);
+        win_area.y = frameExtentY (c);
+        win_area.width = frameExtentWidth (c);
+        win_area.height = frameExtentHeight (c);
+
+        if (!gdk_rectangle_intersect (&win_area, &workarea, NULL))
+        {
+             continue;
+        }
+
         if (strutsToRectangles (c, &left, &right, &top, &bottom))
         {
             /*
@@ -565,29 +576,25 @@ workspaceUpdateArea (ScreenInfo *screen_info)
              * Mimic this behaviour by ignoring struts not on the primary
              * display when calculating NET_WORKAREA
              */
-            if (checkValidStruts (&left, &workarea, STRUTS_LEFT) &&
-                gdk_rectangle_intersect (&left, &workarea, NULL))
+            if (gdk_rectangle_intersect (&left, &workarea, NULL))
             {
                 screen_info->margins[STRUTS_LEFT] = MAX(screen_info->margins[STRUTS_LEFT],
                                                          c->struts[STRUTS_LEFT]);
             }
 
-            if (checkValidStruts (&right, &workarea, STRUTS_RIGHT) &&
-                gdk_rectangle_intersect (&right, &workarea, NULL))
+            if (gdk_rectangle_intersect (&right, &workarea, NULL))
             {
                 screen_info->margins[STRUTS_RIGHT] = MAX(screen_info->margins[STRUTS_RIGHT],
                                                          c->struts[STRUTS_RIGHT]);
             }
 
-            if (checkValidStruts (&top, &workarea, STRUTS_TOP) &&
-                gdk_rectangle_intersect (&top, &workarea, NULL))
+            if (gdk_rectangle_intersect (&top, &workarea, NULL))
             {
                 screen_info->margins[STRUTS_TOP] = MAX(screen_info->margins[STRUTS_TOP],
                                                        c->struts[STRUTS_TOP]);
             }
 
-            if (checkValidStruts (&bottom, &workarea, STRUTS_BOTTOM) &&
-                gdk_rectangle_intersect (&bottom, &workarea, NULL))
+            if (gdk_rectangle_intersect (&bottom, &workarea, NULL))
             {
                 screen_info->margins[STRUTS_BOTTOM] = MAX(screen_info->margins[STRUTS_BOTTOM],
                                                           c->struts[STRUTS_BOTTOM]);

src/client.c
@@ -3310,7 +3310,7 @@ clientNewTileSize (Client *c, XWindowChanges *wc, GdkRectangle *rect, tilePositi
                   rect->x + rect->width) - full_x;
     full_h = MIN (screen_info->height - screen_info->params->xfwm_margins[STRUTS_BOTTOM],
                   rect->y + rect->height) - full_y;
-    clientMaxSpace (screen_info, &full_x, &full_y, &full_w, &full_h);
+    clientMaxSpace (c, &full_x, &full_y, &full_w, &full_h);
 
     switch (tile)
     {
@@ -3384,7 +3384,7 @@ clientNewMaxSize (Client *c, XWindowChanges *wc, GdkRectangle *rect)
                   rect->x + rect->width) - full_x;
     full_h = MIN (screen_info->height - screen_info->params->xfwm_margins[STRUTS_BOTTOM],
                   rect->y + rect->height) - full_y;
-    clientMaxSpace (screen_info, &full_x, &full_y, &full_w, &full_h);
+    clientMaxSpace (c, &full_x, &full_y, &full_w, &full_h);
 
     if (FLAG_TEST (c->flags, CLIENT_FLAG_MAXIMIZED_HORIZ))
     {

src/placement.c
@@ -40,7 +40,6 @@
 #include "frame.h"
 #include "netwm.h"
 
-#define MAX_VALID_STRUT(n) (n / 4) /* 25% of available space */
 #define USE_CLIENT_STRUTS(c) (FLAG_TEST (c->xfwm_flags, XFWM_FLAG_VISIBLE) && \
                               FLAG_TEST (c->flags, CLIENT_FLAG_HAS_STRUT))
 
@@ -141,60 +140,79 @@ strutsToRectangles (Client *c,
     return TRUE;
 }
 
-gboolean
-checkValidStruts (GdkRectangle *struts,
-                  GdkRectangle *monitor,
-                  int side)
+static gboolean
+ClientsOnSameMonitor (ScreenInfo *screen_info, Client *c1, Client *c2)
 {
-    GdkRectangle intersect;
+    GdkRectangle monitor;
+    GdkRectangle win1;
+    GdkRectangle win2;
+    int num_monitors, i;
 
-    if (gdk_rectangle_intersect (struts, monitor, &intersect))
+    num_monitors = xfwm_get_n_monitors (screen_info->gscr);
+
+    set_rectangle (&win1,
+                   frameExtentX (c1),
+                   frameExtentY (c1),
+                   frameExtentWidth (c1),
+                   frameExtentHeight (c1));
+
+    set_rectangle (&win2,
+                   frameExtentX (c2),
+                   frameExtentY (c2),
+                   frameExtentWidth (c2),
+                   frameExtentHeight (c2));
+
+    num_monitors = xfwm_get_n_monitors (screen_info->gscr);
+    for (i = 0; i < num_monitors; i++)
     {
-        switch (side)
+        xfwm_get_monitor_geometry (screen_info->gscr, i, &monitor, FALSE);
+        if (gdk_rectangle_intersect (&win1, &monitor, NULL) &&
+            gdk_rectangle_intersect (&win2, &monitor, NULL))
         {
-            case STRUTS_LEFT:
-            case STRUTS_RIGHT:
-                return (intersect.width < MAX_VALID_STRUT(monitor->width));
-                break;
-            case STRUTS_TOP:
-            case STRUTS_BOTTOM:
-                return (intersect.height < MAX_VALID_STRUT(monitor->height));
-                break;
-            default:
-                TRACE ("unhandled strut side");
-                break;
+            return TRUE;
         }
     }
-    return TRUE;
+
+    return FALSE;
 }
 
 void
-clientMaxSpace (ScreenInfo *screen_info, int *x, int *y, int *w, int *h)
+clientMaxSpace (Client *c, int *x, int *y, int *w, int *h)
 {
-    Client *c;
+    ScreenInfo *screen_info;
+    Client *c2;
     guint i;
     GdkRectangle top, left, right, bottom, area, initial, intersect;
 
+    g_return_if_fail (c != NULL);
     g_return_if_fail (x != NULL);
     g_return_if_fail (y != NULL);
     g_return_if_fail (w != NULL);
     g_return_if_fail (h != NULL);
 
+    TRACE ("client \"%s\" (0x%lx) %s", c->name, c->window);
+
+    screen_info = c->screen_info;
+
     set_rectangle (&area, *x, *y, *w, *h);
     set_rectangle (&initial, *x, *y, *w, *h);
 
-    for (c = screen_info->clients, i = 0; i < screen_info->client_count; c = c->next, i++)
+    for (c2 = screen_info->clients, i = 0; i < screen_info->client_count; c2 = c2->next, i++)
     {
-        if (!USE_CLIENT_STRUTS(c))
+        if (!USE_CLIENT_STRUTS (c2))
         {
             continue;
         }
 
-        if (strutsToRectangles (c, &left, &right, &top, &bottom))
+        if (!ClientsOnSameMonitor (screen_info, c, c2))
+        {
+            continue;
+        }
+
+        if (strutsToRectangles (c2, &left, &right, &top, &bottom))
         {
             /* Left */
-            if (checkValidStruts (&left, &initial, STRUTS_LEFT) &&
-                gdk_rectangle_intersect (&left, &area, &intersect))
+            if (gdk_rectangle_intersect (&left, &area, &intersect))
             {
                 *x = *x + intersect.width;
                 *w = *w - intersect.width;
@@ -202,16 +220,14 @@ clientMaxSpace (ScreenInfo *screen_info, int *x, int *y, int *w, int *h)
             }
 
             /* Right */
-            if (checkValidStruts (&right, &initial, STRUTS_RIGHT) &&
-                gdk_rectangle_intersect (&right, &area, &intersect))
+            if (gdk_rectangle_intersect (&right, &area, &intersect))
             {
                 *w = *w - intersect.width;
                 set_rectangle (&area, *x, *y, *w, *h);
             }
 
             /* Top */
-            if (checkValidStruts (&top, &initial, STRUTS_TOP) &&
-                gdk_rectangle_intersect (&top, &area, &intersect))
+            if (gdk_rectangle_intersect (&top, &area, &intersect))
             {
                 *y = *y + intersect.height;
                 *h = *h - intersect.height;
@@ -219,8 +235,7 @@ clientMaxSpace (ScreenInfo *screen_info, int *x, int *y, int *w, int *h)
             }
 
             /* Bottom */
-            if (checkValidStruts (&bottom, &initial, STRUTS_BOTTOM) &&
-                gdk_rectangle_intersect (&bottom, &area, &intersect))
+            if (gdk_rectangle_intersect (&bottom, &area, &intersect))
             {
                 *h = *h - intersect.height;
                 set_rectangle (&area, *x, *y, *w, *h);
@@ -296,9 +311,13 @@ clientConstrainPos (Client * c, gboolean show_full)
                 continue;
             }
 
+            if (!ClientsOnSameMonitor(screen_info, c, c2))
+            {
+                continue;
+            }
+
             /* right */
-            if (checkValidStruts (&right, &monitor, STRUTS_RIGHT) &&
-                gdk_rectangle_intersect (&right, &win, NULL))
+            if (gdk_rectangle_intersect (&right, &win, NULL))
             {
                 c->x = screen_width - c2->struts[STRUTS_RIGHT] - win.width + frame_left;
                 win.x = frameExtentX (c);
@@ -306,8 +325,7 @@ clientConstrainPos (Client * c, gboolean show_full)
             }
 
             /* Bottom */
-            if (checkValidStruts (&bottom, &monitor, STRUTS_BOTTOM) &&
-                gdk_rectangle_intersect (&bottom, &win, NULL))
+            if (gdk_rectangle_intersect (&bottom, &win, NULL))
             {
                 c->y = screen_height - c2->struts[STRUTS_BOTTOM] - win.height + frame_top;
                 win.y = frameExtentY (c);
@@ -347,9 +365,13 @@ clientConstrainPos (Client * c, gboolean show_full)
                 continue;
             }
 
+            if (!ClientsOnSameMonitor(screen_info, c, c2))
+            {
+                continue;
+            }
+
             /* Left */
-            if (checkValidStruts (&left, &monitor, STRUTS_LEFT) &&
-                gdk_rectangle_intersect (&left, &win, NULL))
+            if (gdk_rectangle_intersect (&left, &win, NULL))
             {
                 c->x = c2->struts[STRUTS_LEFT] + frame_left;
                 win.x = frameExtentX (c);
@@ -357,8 +379,7 @@ clientConstrainPos (Client * c, gboolean show_full)
             }
 
             /* Top */
-            if (checkValidStruts (&top, &monitor, STRUTS_TOP) &&
-                gdk_rectangle_intersect (&top, &win, NULL))
+            if (gdk_rectangle_intersect (&top, &win, NULL))
             {
                 c->y = c2->struts[STRUTS_TOP] + frame_top;
                 win.y = frameExtentY (c);
@@ -407,9 +428,13 @@ clientConstrainPos (Client * c, gboolean show_full)
                 continue;
             }
 
+            if (!ClientsOnSameMonitor(screen_info, c, c2))
+            {
+                continue;
+            }
+
             /* Right */
-            if (checkValidStruts (&right, &monitor, STRUTS_RIGHT) &&
-                gdk_rectangle_intersect (&right, &win, NULL))
+            if (gdk_rectangle_intersect (&right, &win, NULL))
             {
                 if (win.x >= screen_width - c2->struts[STRUTS_RIGHT] - min_visible)
                 {
@@ -420,8 +445,7 @@ clientConstrainPos (Client * c, gboolean show_full)
             }
 
             /* Left */
-            if (checkValidStruts (&left, &monitor, STRUTS_LEFT) &&
-                gdk_rectangle_intersect (&left, &win, NULL))
+            if (gdk_rectangle_intersect (&left, &win, NULL))
             {
                 if (win.x + win.width <= c2->struts[STRUTS_LEFT] + min_visible)
                 {
@@ -432,8 +456,7 @@ clientConstrainPos (Client * c, gboolean show_full)
             }
 
             /* Bottom */
-            if (checkValidStruts (&bottom, &monitor, STRUTS_BOTTOM) &&
-                gdk_rectangle_intersect (&bottom, &win, NULL))
+            if (gdk_rectangle_intersect (&bottom, &win, NULL))
             {
                 if (win.y >= screen_height - c2->struts[STRUTS_BOTTOM] - min_visible)
                 {
@@ -444,8 +467,7 @@ clientConstrainPos (Client * c, gboolean show_full)
             }
 
             /* Top */
-            if (checkValidStruts (&top, &monitor, STRUTS_TOP) &&
-                gdk_rectangle_intersect (&top, &win, NULL))
+            if (gdk_rectangle_intersect (&top, &win, NULL))
             {
                 if (segment_overlap (win.y, win.y + title_visible, 0, c2->struts[STRUTS_TOP]))
                 {
@@ -812,7 +834,7 @@ clientInitPosition (Client * c)
                   rect.y + rect.height) - full_y;
 
     /* Adjust size to the widest size available, not covering struts */
-    clientMaxSpace (screen_info, &full_x, &full_y, &full_w, &full_h);
+    clientMaxSpace (c, &full_x, &full_y, &full_w, &full_h);
 
     /*
        If the windows is smaller than the given ratio of the available screen area,
@@ -1005,19 +1027,19 @@ clientFill (Client * c, int fill_type)
     {
         mask = CWX | CWY | CWHeight | CWWidth;
         /* Adjust size to the largest size available, not covering struts */
-        clientMaxSpace (screen_info, &full_x, &full_y, &full_w, &full_h);
+        clientMaxSpace (c, &full_x, &full_y, &full_w, &full_h);
     }
     else if (fill_type & CLIENT_FILL_VERT)
     {
         mask = CWY | CWHeight;
         /* Adjust size to the tallest size available, for the current horizontal position/width */
-        clientMaxSpace (screen_info, &tmp_x, &full_y, &tmp_w, &full_h);
+        clientMaxSpace (c, &tmp_x, &full_y, &tmp_w, &full_h);
     }
     else if (fill_type & CLIENT_FILL_HORIZ)
     {
         mask = CWX | CWWidth;
         /* Adjust size to the widest size available, for the current vertical position/height */
-        clientMaxSpace (screen_info, &full_x, &tmp_y, &full_w, &tmp_h);
+        clientMaxSpace (c, &full_x, &tmp_y, &full_w, &tmp_h);
     }
 
     /* If there are neighbours, resize to their borders.

src/placement.h
@@ -40,10 +40,7 @@ gboolean                 strutsToRectangles                     (Client *,
                                                                  GdkRectangle * /* right */,
                                                                  GdkRectangle * /* top */,
                                                                  GdkRectangle * /* bottom */);
-gboolean                 checkValidStruts                       (GdkRectangle * /* struts */,
-                                                                 GdkRectangle * /* monitor */,
-                                                                 int);
-void                     clientMaxSpace                         (ScreenInfo *,
+void                     clientMaxSpace                         (Client *,
                                                                  int *,
                                                                  int *,
                                                                  int *,

commit 182bd22c4675150497aa4e3de1348a97f7277461

workspace: Update maximized on struts change
Since commit 7e8d60bb (Ignore struts from windows on other monitors),
the work area is limited to the primary monitor ony.

As a result, when a panel is placed on a non-primary output, the workarea
does not actually change.

But we would update the maximized window size and position only when the
workarea changed, which is now wrong, we need to update any time the
struts change.

Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/658
Fixes: 7e8d60bb
Signed-off-by: Olivier Fourdan <fourdan@xfce.org>

src/workspaces.c
@@ -610,7 +610,8 @@ workspaceUpdateArea (ScreenInfo *screen_info)
         TRACE ("margins have changed, updating net_workarea");
         setNetWorkarea (display_info, screen_info->xroot, screen_info->workspace_count,
                         screen_info->width, screen_info->height, screen_info->margins);
-        /* Also prevent windows from being off screen, just like when screen is resized */
-        clientScreenResize(screen_info, FALSE);
     }
+
+    /* Also prevent windows from being off screen, just like when screen is resized */
+    clientScreenResize(screen_info, FALSE);
 }