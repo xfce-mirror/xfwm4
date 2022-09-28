0d8df70fd4208329e203e195e8a18f2721116353
Enrico Weigelt, metux IT consult <info@metux.net>
2022-09-28 08:58:03 +0000

    introduce window fences
    This new feature allows defining screen regions that act like fences around
    grassland, so cattle remains confined in there: when a window is moved over
    some fenced area, it will snap into it and expand to the fenced region.
    
    The window later can be moved out to an unfenced area, where it reverts to
    normal behaviour, or moved into another one.
    
    Configuration is done via xfconf in the xfwm4 channel:
    
      |
      +-- "fences" [empty/array]
      |   |
      |   +-- <ident1> [array]
      |   |   |
      |   |   +-- "title" [string]
      |   |   +-- "geometry"         [string]  -- X geometry string (see XParseGeometry)
      |   |   +-- "border-color"     [string]  -- Gdk color string for border
      |   |   +-- "background-color" [string]  -- Gdk color string for background
      |   |   +-- "auto-maximize"    [boolean] -- auto-maximize on entering fence
      |   +-- <ident> [array]
      |   |   | ...
    
    X-Feature-Add: window-fence
    X-Feature-Depend: list.h
    X-Feature-Depend: xfwm_xfconf_get_geometry
    X-Feature-Depend: client_is_maximized
    X-Feature-Depend: window-properties
    X-Feature-Depend: record-window-type-name
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- src/moveresize.c
+++ src/moveresize.c
@@ -52,6 +52,7 @@
 #include "wireframe.h"
 #include "workspaces.h"
 #include "xsync.h"
+#include "fences.h"
 
 #define MOVERESIZE_POINTER_EVENT_MASK \
     PointerMotionMask | \
@@ -91,6 +92,7 @@ struct _MoveResizeData
     gint oldw, oldh;
     gint handle;
     Poswin *poswin;
+    FenceInfo *fence;
 };
 
 static int
@@ -879,6 +881,7 @@ clientMoveEventFilter (XfwmEvent *event, gpointer data)
     int prev_x, prev_y;
     unsigned long cancel_maximize_flags;
     unsigned long cancel_restore_size_flags;
+    FenceInfo *new_fence;
 
     TRACE ("entering");
 
@@ -887,6 +890,13 @@ clientMoveEventFilter (XfwmEvent *event, gpointer data)
     prev_y=c->y;
     screen_info = c->screen_info;
     display_info = screen_info->display_info;
+
+    new_fence = fencesFindBest (&screen_info->window_fences, frameExtentGeometry (c));
+    if (new_fence != passdata->fence) {
+        fenceHide (passdata->fence);
+        fenceShow (new_fence, screen_info);
+        passdata->fence = new_fence;
+    }
 
     /*
      * Clients may choose to end the move operation,
@@ -1143,7 +1153,7 @@ clientMove (Client * c, XfwmEventButton *event)
     ScreenInfo *screen_info;
     DisplayInfo *display_info;
     XWindowChanges wc;
-    MoveResizeData passdata;
+    MoveResizeData passdata = { 0 };
     int changes;
     gboolean g1, g2;
 
@@ -1183,7 +1193,9 @@ clientMove (Client * c, XfwmEventButton *event)
     passdata.is_transient = clientIsValidTransientOrModal (c);
     passdata.move_resized = FALSE;
     passdata.wireframe = NULL;
+    passdata.fence = c->window_fence.fence;
 
+    fenceShow (passdata.fence, screen_info);
     clientSaveSizePos (c);
 
     if (event && event->pressed)
@@ -1256,6 +1268,9 @@ clientMove (Client * c, XfwmEventButton *event)
         goto move_cleanup;
     }
     FLAG_UNSET (c->xfwm_flags, XFWM_FLAG_MOVING_RESIZING);
+
+    clientSetFence (c, passdata.fence);
+    fenceHide (passdata.fence);
 
     if (passdata.grab && screen_info->params->box_move)
     {
