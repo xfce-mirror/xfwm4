9865844ba076ea19fbdbd6158b3e3d1daeec693a
Enrico Weigelt, metux IT consult <info@metux.net>
2022-09-28 08:58:03 +0000

    introduce window fences
    This new feature allows defining screen regions that act like fences around
    grassland, so cattle remains confined in there: when a window is moved over
    some fenced area, it will snap into it and expand to the fenced region.
    
    The window later can be moved out to an unfenced area, where it reverts to
    normal behaviour, or moved into another one.
    
    Configuration is done via xfconf in the xfwm4 channel:
    
      |
      +-- "fence" [empty/array]
      |   |
      |   +-- <ident1> [array]
      |   |   |
      |   |   +-- "title" [string]
      |   |   +-- "geometry"         [string]  -- X geometry string (see XParseGeometry)
      |   |   +-- "border-color"     [string]  -- Gdk color string for border
      |   |   +-- "background-color" [string]  -- Gdk color string for background
      |   |   +-- "auto-maximize"    [boolean] -- auto-maximize on entering fence
      |   +-- <ident> [array]
      |   |   | ...
    
    X-Feature-Add: window-fence
    X-Feature-Depend: list.h
    X-Feature-Depend: xfwm_xfconf_get_geometry
    X-Feature-Depend: client_is_maximized
    X-Feature-Depend: window-properties
    X-Feature-Depend: record-window-type-name
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

---
+++ src/fences.h
@@ -0,0 +1,36 @@
+/* X-SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __XFWM4_FENCES_H
+#define __XFWM4_FENCES_H
+
+#include <gdk/gdk.h>
+#include <xfconf/xfconf.h>
+
+#include <common/list.h>
+
+typedef struct _ScreenInfo ScreenInfo;
+typedef struct _FenceInfo FenceInfo;
+typedef struct _WireFrame WireFrame;
+
+struct _FenceInfo {
+    struct list_head list;
+
+    GdkRectangle geometry;
+    gchar *ident;
+    gchar *title;
+    GdkRGBA border_color;
+    GdkRGBA background_color;
+    WireFrame *wireframe;
+    gboolean auto_maximize;
+};
+
+void       fencesFree(struct list_head *fences) NONNULL(1);
+int        fencesLoad(struct list_head *fences, XfconfChannel *channel) NONNULL(1,2);
+FenceInfo* fencesLookup(struct list_head *fences, const gchar *ident) NONNULL(1);
+
+FenceInfo* fencesFindBest(struct list_head *fences, GdkRectangle geometry) NONNULL(1);
+
+void       fenceShow(FenceInfo *fence, ScreenInfo *screen_info) NONNULL(2);
+void       fenceHide(FenceInfo *fence);
+
+#endif /* __XFWM4_FENCES_H */
