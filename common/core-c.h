1e5e7a47e43db9ad00f19f6560a600ae6cb456b2
Enrico Weigelt, metux IT consult <info@metux.net>
2022-09-28 11:01:59 +0000

    common: core-c.h: add container_of() macro
    Helpful for working with nested structs: get the pointer to the
    container struct from pointer to a contained one. Subsequent patches
    will use this e.g. for generic list operations.
    
    X-Feature-Depend: core-c.h
    X-Feature-Add: container_of
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- common/core-c.h
+++ common/core-c.h
@@ -23,4 +23,15 @@
 #define NONNULL(...)
 #endif
 
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:        the pointer to the member.
+ * @type:       the type of the container struct this is embedded in.
+ * @member:     the name of the member within the struct.
+ *
+ */
+#define container_of(ptr, type, member) ({          \
+    char *__mptr = (char *)(ptr);                   \
+    ((type *)(__mptr - offsetof(type, member))); })
+
 #endif /* __XFWM_CORE_C_H */
