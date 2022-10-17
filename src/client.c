13e3b9fc704b47d5c9b293444877475725328401
Enrico Weigelt, metux IT consult <info@metux.net>
2022-10-17 08:15:29 +0000

    policy: add forcing borders around windows
    Adding a policy "force.border" for forcing a window to have frame,
    no matter what the application says. This gives users the choice to
    override applications if he doesn't want borderless windows.
    
    Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/552
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- src/client.c
+++ src/client.c
@@ -56,6 +56,7 @@
 #include "mywindow.h"
 #include "netwm.h"
 #include "placement.h"
+#include "policy.h"
 #include "screen.h"
 #include "session.h"
 #include "settings.h"
@@ -1853,6 +1854,12 @@ clientFrame (DisplayInfo *display_info, Window w, gboolean recapture)
         {
             clientInitPosition (c);
         }
+    }
+
+    /* and now let's apply other policies */
+    if (clientPolicyGetBool (c, "force.border", FALSE))
+    {
+        FLAG_SET (c->xfwm_flags, XFWM_FLAG_HAS_BORDER);
     }
 
     /*
