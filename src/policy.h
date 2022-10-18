2bf3ef22bb9fd1ee9a467e25d8b2fb309a3c4d8c
Enrico Weigelt, metux IT consult <info@metux.net>
2022-10-18 18:03:10 +0200

    policies: make the feature build-time configurable
    Built the window policies feature only if --enable-window-polcies
    passed to configure.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- src/policy.h
+++ src/policy.h
@@ -7,6 +7,8 @@
 
 #include <common/core-c.h>
 
+#ifdef ENABLE_WINDOW_POLICIES
+
 gchar * policy_get_string(XfconfChannel *channel,
                           const gchar *property,
                           const gchar *res_class,
@@ -21,5 +23,13 @@ int policy_get_geometry(XfconfChannel *channel,
                         const gchar *wm_name,
                         const gchar *window_type,
                         GdkRectangle *geometry) NONNULL(1,2,7);
+
+#else /* ENABLE_WINDOW_POLICIES */
+
+#define policy_get_string(a,b,c,d,e,f)        (NULL)
+#define policy_get_bool(a,b,c,d,e,f,g)        (FALSE)
+#define policy_get_geometry(a,b,c,d,e,f,g)    (0)
+
+#endif /* ENABLE_WINDOW_POLICIES */
 
 #endif /* __XFWM4_POLICY_H */
