999ef24c78a0ba5343011ace6cb80c01b6c38fc8
Enrico Weigelt, metux IT consult <info@metux.net>
2022-10-20 14:57:30 +0000

    wireframe: add window class hint to wireframes
    This allows other programs, eg. external compositors, to properly identify
    wireframes.
    
    Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/208
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- src/wireframe.c
+++ src/wireframe.c
@@ -191,6 +191,19 @@ wireframeInitColor (WireFrame *wireframe)
     }
 }
 
+static void wireframeSetHints (WireFrame *wireframe)
+{
+    XClassHint *hint = XAllocClassHint();
+
+    hint->res_name = "xfwm4-wireframe";
+    hint->res_class = "Xfwm4";
+
+    XSetClassHint (myScreenGetXDisplay(wireframe->screen_info),
+                   wireframe->xwindow, hint);
+
+    XFree(hint);
+}
+
 WireFrame *
 wireframeCreate (Client *c)
 {
@@ -257,6 +270,7 @@ wireframeCreate (Client *c)
         cairo_set_line_join (wireframe->cr, CAIRO_LINE_JOIN_MITER);
     }
 
+    wireframeSetHints (wireframe);
     wireframeUpdate (c, wireframe);
 
     return (wireframe);
