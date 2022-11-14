xfce-mirror/xfwm4

commit 182bd22c4675150497aa4e3de1348a97f7277461

workspace: Update maximized on struts change
Since commit 7e8d60bb (Ignore struts from windows on other monitors),
the work area is limited to the primary monitor ony.

As a result, when a panel is placed on a non-primary output, the workarea
does not actually change.

But we would update the maximized window size and position only when the
workarea changed, which is now wrong, we need to update any time the
struts change.

Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/658
Fixes: 7e8d60bb
Signed-off-by: Olivier Fourdan <fourdan@xfce.org>

src/workspaces.c
@@ -610,7 +610,8 @@ workspaceUpdateArea (ScreenInfo *screen_info)
         TRACE ("margins have changed, updating net_workarea");
         setNetWorkarea (display_info, screen_info->xroot, screen_info->workspace_count,
                         screen_info->width, screen_info->height, screen_info->margins);
-        /* Also prevent windows from being off screen, just like when screen is resized */
-        clientScreenResize(screen_info, FALSE);
     }
+
+    /* Also prevent windows from being off screen, just like when screen is resized */
+    clientScreenResize(screen_info, FALSE);
 }

commit ca9c1aa8ec4ac7d1e70fcd8da4d69c1912172e7b

client: Fix
clientScreenResize() ensures that tiled, fullscreen and maximized wintod
remain correctly sized when the overall screen size is changed, but also
tries to save and restore window positions in case of relayouts.

Since commit 182bd22c „Update maximized on struts change“ however, we
all clientScreenResize() from workspaceUpdateArea() unconditionally
while workspaceUpdateArea() can be called every time a window is moved.

That causes a regression when moving undecorated windows, typically
those meant to remain fully visible on screen when screen size changes,
and every other move would save and restore the previous position,
causing spurious and unexpected window movements.

To solve the problem, add a new parameter to clientScreenResize() to
specify if the all is originating from a relayout.

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>
Fixes: 182bd22c - Update maximized on struts change
Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/677

src/client.c
@@ -4015,7 +4015,7 @@ clientIncOpacity (Client *c)
 
 /* Xrandr stuff: on screen size change, make sure all clients are still visible */
 void
-clientScreenResize(ScreenInfo *screen_info, gboolean fully_visible)
+clientScreenResize(ScreenInfo *screen_info, gboolean fully_visible, gboolean relayout)
 {
     Client *c = NULL;
     GList *list, *list_of_windows;
@@ -4059,7 +4059,7 @@ clientScreenResize(ScreenInfo *screen_info, gboolean fully_visible)
         {
             clientUpdateTileSize (c);
         }
-        else
+        else if (relayout)
         {
             configure_flags = CFG_CONSTRAINED | CFG_REQUEST;
             if (fully_visible)

src/client.h
@@ -491,6 +491,7 @@ void                     clientDecOpacity                       (Client *);
 void                     clientUpdateCursor                     (Client *);
 void                     clientUpdateAllCursor                  (ScreenInfo *);
 void                     clientScreenResize                     (ScreenInfo *,
+                                                                 gboolean,
                                                                  gboolean);
 void                     clientButtonPress                      (Client *,
                                                                  Window,

src/events.c
@@ -2778,7 +2778,7 @@ size_changed_cb(GdkScreen *gscreen, gpointer data)
         compositorUpdateScreenSize (screen_info);
     }
 
-    clientScreenResize (screen_info, FALSE);
+    clientScreenResize (screen_info, FALSE, TRUE);
 }
 
 /*
@@ -2836,7 +2836,7 @@ monitors_changed_cb(GdkScreen *gscreen, gpointer data)
         compositorUpdateScreenSize (screen_info);
     }
 
-    clientScreenResize (screen_info, (screen_info->num_monitors < previous_num_monitors));
+    clientScreenResize (screen_info, (screen_info->num_monitors < previous_num_monitors), TRUE);
 }
 
 void

src/workspaces.c
@@ -613,5 +613,5 @@ workspaceUpdateArea (ScreenInfo *screen_info)
     }
 
     /* Also prevent windows from being off screen, just like when screen is resized */
-    clientScreenResize(screen_info, FALSE);
+    clientScreenResize(screen_info, FALSE, FALSE);
 }