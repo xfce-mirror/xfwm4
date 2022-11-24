xfce-mirror/xfwm4

commit f68c04ba8591615e5ee615865daf50c23b35ef33

startup-notification: Crash/stack smash on some architectures
Bug: 15028

sn_startup_sequence_get_last_active_time takes two long pointer
arguments, but xfwm4 passes pointers to may-be-different types.

Signed-off-by: Martin Husemann <martin@NetBSD.org>

src/startup_notification.c
@@ -128,6 +128,7 @@ sn_collect_timed_out_foreach (void *element, void *data)
     SnStartupSequence *sequence;
     time_t tv_sec;
     suseconds_t tv_usec;
+    long l_sec, l_usec;
     double elapsed;
 
     g_return_if_fail (data != NULL);
@@ -135,7 +136,8 @@ sn_collect_timed_out_foreach (void *element, void *data)
 
     sequence = element;
     ctod = (CollectTimedOutData *) data;
-    sn_startup_sequence_get_last_active_time (sequence, &tv_sec, &tv_usec);
+    sn_startup_sequence_get_last_active_time (sequence, &l_sec, &l_usec);
+    tv_sec = l_sec; tv_usec = l_sec;
 
     elapsed =
         ((((double) ctod->now.tv_sec - tv_sec) * G_USEC_PER_SEC +

commit 94c6b2507e771aaf3762fb15291c0b8b04e9c3a1

startup-notification: Fix timeout computation
Commit f68c04ba8 "Crash/stack smash on some architectures" had a typo
that would mix the values of usecs and secs, causing a very long busy
cursor when the startup notification never arrives.

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>
Fixes: f68c04ba8 - "Crash/stack smash on some architectures"
Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/518

src/startup_notification.c
@@ -137,7 +137,7 @@ sn_collect_timed_out_foreach (void *element, void *data)
     sequence = element;
     ctod = (CollectTimedOutData *) data;
     sn_startup_sequence_get_last_active_time (sequence, &l_sec, &l_usec);
-    tv_sec = l_sec; tv_usec = l_sec;
+    tv_sec = l_sec; tv_usec = l_usec;
 
     elapsed = ((double) ctod->now - (tv_sec * G_USEC_PER_SEC) - tv_usec)
         / 1000.0;