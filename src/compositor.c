xfce-mirror/xfwm4

commit 0bacec27bd35843dc4994fc0f53191c604ac566b

compositor: Damage window when adding the opaque region
When mapping a new window for the first time, the client may set an
opaque region prior to draw anything.

As opaquere regions are meant to hint the compositor that anything
underneath doesn't need updating (as hidden behind an opaque region,
precisely), xfwm4 would leave that region untouched.

However, when using double buffering as with xpresent, it means that
content of the different buffers may end up being different if there is
any damage occurring underneath the opaque region, before the client
itself has painted anything.

As the buffers alternate quickly, the content behind the opaque region
will show alternatively which is ugly (although technically correct).

To avoid that issue, when setting the opaque region first, make sure to
damage the whole window so that the content of the different buffers
remain updated even if behind the opaque region.

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>
Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/583
Fixes: 50d34206 - Damage opaque region if changed

src/compositor.c
@@ -3268,6 +3268,10 @@ update_opaque_region (CWindow *cw, Window id)
             add_damage (screen_info, old_opaque_region);
         }
     }
+    else
+    {
+        damage_win (cw);
+    }
 }
 
 static void

commit c4920390ecebdaae70bba4f5a718eb027304204e

compositor: Expose just the window area
When a client sets its opaque area for the first time, xfwm4 would
damage the entire window, which in turn will create the drop shadows
picture with server side decorations.

This is however racy and can sometimes prevent the shadows from being
visible when the window is mapped at first.

To avoid that issue, we just expose the area for the window alone, not
going through the full process of creating the drop shadow.

Signed-off-by: Olivier Fourdan <fourdan@xfce.org>
Fixes: 0bacec27 - Damage window when adding the opaque region
Closes: https://gitlab.xfce.org/xfce/xfwm4/-/issues/696

src/compositor.c
@@ -3294,7 +3294,14 @@ update_opaque_region (CWindow *cw, Window id)
     }
     else
     {
-        damage_win (cw);
+        XRectangle rect[1];
+
+        rect[0].x = cw->attr.x;
+        rect[0].y = cw->attr.y;
+        rect[0].width = cw->attr.width + 2 * cw->attr.border_width;
+        rect[0].height = cw->attr.height + 2 * cw->attr.border_width;
+
+        expose_area (screen_info, rect, 1);
     }
 }
 