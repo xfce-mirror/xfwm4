980fc7c47438bed815db9fa5250647bf88900c41
Gaël Bonithon <gael@xfce.org>
2024-06-04 22:19:56 +0200

    build: clang: Fix -Wsingle-bit-bitfield-constant-conversion

--- settings-dialogs/range-debouncer.c
+++ settings-dialogs/range-debouncer.c
@@ -32,7 +32,7 @@ struct _RangeDebouncer
   GObject              __parent__;
 
   GtkRange            *range;
-  gboolean             pressed : 1;
+  guint                pressed : 1;
 
   gulong               signal1;
   gulong               signal2;
